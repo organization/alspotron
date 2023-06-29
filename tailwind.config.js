ee4991b4faba8e622e64bf31a89efc9915688acc
Su-Yong <simssy2205@gmail.com>
2023-06-29 17:30:33 +0900

    feat: 기본 크기 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,17 @@ module.exports = {
       error: colors.red,
       warn: colors.yellow,
     },
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '0.875rem',
+      lg: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
   },
   plugins: [],
 }
