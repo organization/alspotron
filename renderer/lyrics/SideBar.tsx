c614e3db3037df9232c58e250a893dfdcd777402
nenw <khi@nenw.dev>
2023-07-01 22:45:18 +0900

    feat: add lyric viewer in SideBar

--- renderer/lyrics/SideBar.tsx
+++ renderer/lyrics/SideBar.tsx
@@ -56,6 +56,9 @@ const SideBar = () => {
           <path d="M8.293 4.293a1 1 0 0 0 0 1.414L14.586 12l-6.293 6.293a1 1 0 1 0 1.414 1.414l7-7a1 1 0 0 0 0-1.414l-7-7a1 1 0 0 0-1.414 0Z" fill="#ffffff"/>
         </svg>
       </Card>
+      <div class={'flex-1 block text-center overflow-scroll whitespace-pre-line'}>
+        {lyricItems()}
+      </div>
     </div>
   )
 };
