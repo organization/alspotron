98f21ae4a4c2f6ba1b14b86f3e05f624399c0c7d
Kintiru <Kintiru@gmail.com>
2023-07-03 19:58:59 +0900

    chore: remove unnecessary line break

--- renderer/components/Modal.tsx
+++ renderer/components/Modal.tsx
@@ -4,9 +4,7 @@ import { Portal } from 'solid-js/web'
 const Modal = (props: { open?: boolean, closer?: () => void, children: JSX.Element, }) => (
   <Show when={props.open && (document.getElementById('Modal') == null)}>
     <Portal mount={document.getElementById('app')}>
-      <div
-        class={'fixed inset-0 backdrop-blur-sm backdrop-grayscale bg-primary-500/5 w-full h-full'}
-      >
+      <div class={'fixed inset-0 backdrop-blur-sm backdrop-grayscale bg-primary-500/5 w-full h-full'}>
         <div class={'flex w-full h-full justify-center items-center'}>
           {props.children}
         </div>
