652cdef60704c7ea0df418004822a17e6d3aa25c
Su-Yong <simssy2205@gmail.com>
2023-07-03 14:16:20 +0900

    fix(lyric): 검색 버튼 가로크기가 너무 큰 문제 수정

--- renderer/lyrics/App.tsx
+++ renderer/lyrics/App.tsx
@@ -100,7 +100,7 @@ const LyricsMapEditor = () => {
               }
             }}
           />
-          <button class={'btn-text btn-icon'} onClick={() => void onSearch()}>
+          <button class={'btn-text btn-icon !min-w-0'} onClick={() => void onSearch()}>
             <svg width="16" height="16" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M10 2.5a7.5 7.5 0 0 1 5.964 12.048l4.743 4.745a1 1 0 0 1-1.32 1.497l-.094-.083-4.745-4.743A7.5 7.5 0 1 1 10 2.5Zm0 2a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11Z" fill="#ffffff" />
             </svg>
