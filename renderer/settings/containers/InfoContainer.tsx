717c040fc7c7a4bfb81c6b34d6e9b54a21322950
JellyBrick <shlee1503@naver.com>
2023-07-04 01:34:46 +0900

    change: 레거시 업데이트 패널 제거, 크레딧 추가

--- renderer/settings/containers/InfoContainer.tsx
+++ renderer/settings/containers/InfoContainer.tsx
@@ -130,7 +130,7 @@ const InfoContainer = () => {
             Khinenw
           </div>
           <div class={'text-xs text-white/75'}>
-            Alspotify 원작자
+            Alspotify 원작자, Alspotron 개발자
           </div>
         </div>
         <div class={'flex-1'} />
@@ -145,7 +145,7 @@ const InfoContainer = () => {
             Su-Yong
           </div>
           <div class={'text-xs text-white/75'}>
-            Alspotron 제작자
+            Alspotron 개발자
           </div>
         </div>
         <div class={'flex-1'} />
@@ -160,7 +160,22 @@ const InfoContainer = () => {
             JellyBrick
           </div>
           <div class={'text-xs text-white/75'}>
-            Alspotron 제작자
+            Alspotron 개발자
+          </div>
+        </div>
+        <div class={'flex-1'} />
+        <svg width="16" height="16" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M6.25 4.75a1.5 1.5 0 0 0-1.5 1.5v11.5a1.5 1.5 0 0 0 1.5 1.5h11.5a1.5 1.5 0 0 0 1.5-1.5v-4a1 1 0 1 1 2 0v4a3.5 3.5 0 0 1-3.5 3.5H6.25a3.5 3.5 0 0 1-3.5-3.5V6.25a3.5 3.5 0 0 1 3.5-3.5h4a1 1 0 1 1 0 2h-4Zm6.5-1a1 1 0 0 1 1-1h6.5a1 1 0 0 1 1 1v6.5a1 1 0 1 1-2 0V6.164l-4.793 4.793a1 1 0 1 1-1.414-1.414l4.793-4.793H13.75a1 1 0 0 1-1-1Z" class={'fill-white'} />
+        </svg>
+      </Card>
+      <Card class={'flex flex-row justify-start items-center gap-1'} onClick={() => onLink('https://github.com/smnis')}>
+        <img src={'https://avatars.githubusercontent.com/u/13712304?s=64&v=4'} class={'w-6 h-6 mr-4 rounded-full'} alt='smnis Profile Image'/>
+        <div class={'flex flex-col justify-center items-start'}>
+          <div class={''}>
+            SeongMin Park
+          </div>
+          <div class={'text-xs text-white/75'}>
+            Alspotron 개발자
           </div>
         </div>
         <div class={'flex-1'} />
