51eb564d6d0cdad6e79b0538fb33a47e51502d3c
JellyBrick <shlee1503@naver.com>
2023-07-13 23:42:58 +0900

    Use shorter `lorem ipsum` for file size

--- renderer/settings/App.tsx
+++ renderer/settings/App.tsx
@@ -87,15 +87,7 @@ const App = () => {
             </Match>
             <Match when={tabId() === 'plugin'}>
               <div class={'flex-1 fluent-scrollbar'}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec elit sed velit gravida viverra eleifend non diam. Quisque turpis dui, posuere eu accumsan a, porttitor vitae orci. Nullam placerat elementum massa gravida laoreet. In blandit urna sit amet justo ultricies facilisis. Proin eget dictum purus. In condimentum facilisis mauris a pretium. Maecenas sollicitudin arcu id vestibulum fringilla. Maecenas dictum tincidunt nisl eu tristique. Suspendisse potenti. Mauris sit amet augue at purus dictum tempor id sit amet nibh. Aenean quis justo ac sem egestas tristique. Sed purus enim, pellentesque vitae dictum a, pretium vehicula velit.
-
-              Aenean purus ipsum, mattis id ante at, imperdiet semper nisi. Mauris et nisi vulputate, imperdiet ante vitae, aliquam elit. Curabitur rutrum, ante finibus vestibulum ornare, est ligula congue ipsum, ac tincidunt nisi nisl vitae purus. Proin condimentum orci vel justo volutpat ullamcorper. Aliquam in facilisis erat, id auctor erat. Suspendisse convallis facilisis odio, at dictum ex placerat non. Mauris eget augue dapibus, facilisis augue sed, tincidunt ante. Ut non finibus nisl, id vestibulum lorem. Duis iaculis elementum purus, quis convallis nisi tempus sed.
-
-              Sed dictum nec magna a fringilla. Nullam quis venenatis odio, ac vehicula ligula. Suspendisse imperdiet pellentesque cursus. Donec tempus, nunc id lobortis porta, ante sem maximus metus, at hendrerit massa est vel ligula. Vivamus at lacinia augue, vitae elementum augue. Vestibulum rutrum, diam nec molestie dignissim, eros leo faucibus mi, ac posuere ante orci eget est. Duis facilisis a metus vitae vestibulum. Sed laoreet massa convallis commodo ullamcorper. Pellentesque metus leo, faucibus sit amet finibus vitae, dictum vitae lectus. Ut ut rutrum ligula.
-
-              Praesent suscipit sem sed ipsum porttitor pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent ut odio a tellus faucibus dictum non at massa. Phasellus sed neque lacinia, aliquam purus sit amet, suscipit tortor. Etiam ultricies aliquam lectus eu lobortis. Cras ultricies sagittis fermentum. Suspendisse consequat arcu in urna lobortis pellentesque. Aliquam elementum aliquam nulla quis finibus. Mauris ac purus suscipit, imperdiet est a, lacinia purus. Etiam condimentum dapibus eros, ac aliquam dolor condimentum ac. Pellentesque placerat eros ac nisl tempor, a faucibus erat dapibus. Nulla facilisi. Maecenas risus nisl, accumsan in accumsan vel, faucibus sit amet mi.
-
-              Integer sollicitudin risus id lorem congue, quis consequat massa laoreet. Praesent imperdiet justo a odio eleifend, ac vestibulum nisl iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin et laoreet eros, vel lobortis arcu. Donec fermentum placerat risus, sit amet semper neque tincidunt nec. Morbi semper magna ac lorem sagittis sollicitudin. Vestibulum sed ligula at lacus porta egestas in quis nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </div>
             </Match>
           </Switch>
