1d82c076ee737d367ec8809e02630c330d4eb9e6
nenw <khi@nenw.dev>
2023-07-14 18:18:32 +0900

    feat: overhaul transition

--- renderer/main/components/Lyrics.tsx
+++ renderer/main/components/Lyrics.tsx
@@ -1,64 +1,18 @@
-import { createMemo, For, splitProps } from 'solid-js';
-import { TransitionGroup } from 'solid-transition-group'
 
-import LyricsItem from './LyricsItem'
+import { splitProps } from 'solid-js';
+
+import LyricsTransition from './LyricsTransition';
 
 import { usePlayingInfo } from '../../components/PlayingInfoProvider';
-import useConfig from '../../hooks/useConfig';
 import useLyric from '../../hooks/useLyric';
-import { cx } from '../../utils/classNames';
-import { userCSSSelectors, userCSSTransitions } from '../../utils/userCSSSelectors';
 
 import type { JSX } from 'solid-js/jsx-runtime';
 
 const Lyrics = (props: JSX.HTMLAttributes<HTMLDivElement>) => {
-  const [config] = useConfig();
   const [, containerProps] = splitProps(props, ['class']);
   const { status } = usePlayingInfo();
-  const [lyric] = useLyric();
-
-  const animation = () => {
-    const configuredName = config()?.style?.animation ?? 'pretty';
-    if (configuredName === 'custom') {
-      return userCSSTransitions['transition-lyric'];
-    }
-
-    return `lyric-${configuredName}`;
-  };
-
-  const style = createMemo(() => {
-    const result: Record<string, string> = {
-      'text-align': 'var(--text-align)',
-    };
-    const configData = config();
-    
-    if (configData?.style?.font) result['font-family'] = configData?.style.font;
-    if (configData?.style?.fontWeight) result['font-weight'] = configData?.style.fontWeight;
-    if (configData?.style?.lyric?.fontSize) result['font-size'] = `${configData.style.lyric.fontSize}px`;
-    if (configData?.style?.lyric?.color) result['color'] = configData?.style.lyric.color;
-    if (configData?.style?.lyric?.background) result['background-color'] = configData?.style.lyric.background;
-
-    return Object.entries(result).map(([key, value]) => `${key}: ${value};`).join(' ');
-  });
-
-  return (
-    <div class={cx('flex flex-col items-end gap-4', userCSSSelectors.lyrics, props.class)} {...containerProps}>
-      <TransitionGroup name={`${animation()}`}>
-        <For each={lyric() ?? []}>
-          {(item, index) => item && (
-            <LyricsItem
-              class={userCSSSelectors['lyrics-item']}
-              status={status()}
-              delay={animation() === 'none' ? 0 : index()}
-              style={style()}
-            >
-              {item}
-            </LyricsItem>
-          )}
-        </For>
-      </TransitionGroup>
-    </div>
-  );
-};
+  const [lyrics] = useLyric();
+  return <LyricsTransition lyrics={lyrics() ?? []} status={status()} {...containerProps} />
+}
 
 export default Lyrics;
---
+++ renderer/main/components/LyricsTransition.tsx
@@ -0,0 +1,127 @@
+import { createMemo, For, JSX, on, Show, splitProps, untrack } from 'solid-js';
+import { TransitionGroup } from 'solid-transition-group';
+
+import LyricsItem from './LyricsItem';
+
+import { Status } from '../../components/PlayingInfoProvider';
+import useConfig from '../../hooks/useConfig';
+import { cx } from '../../utils/classNames';
+import { userCSSSelectors, userCSSTransitions, userCSSVariables } from '../../utils/userCSSSelectors';
+
+type LyricsProps = {
+  lyrics: string[];
+  status: Status;
+  style: string;
+};
+
+const Lyrics = (props: LyricsProps) => (
+  <For each={props.lyrics}>
+    {(item, index) => item && (
+      <LyricsItem
+        class={userCSSSelectors['lyrics-item']}
+        status={props.status}
+        style={`
+          --order: ${index()};
+          ${userCSSVariables['var-lyric-order']}: var(--order);
+          ${props.style};
+        `}
+      >
+        {item}
+      </LyricsItem>
+    )}
+  </For>
+);
+
+type LyricsTransitionGroupProps = {
+  animation: string;
+  lyrics: string[];
+  lyricsStyle: string;
+  lyricsStatus: Status;
+  container: ({ children }: { children: JSX.Element }) => JSX.Element;
+};
+
+const LyricsTransitionGroupAllAtOnce = (props: LyricsTransitionGroupProps) => {
+  const lyricTransitionGroup = () => [props.lyrics];
+  const Container = untrack(() => props.container);
+
+  return (
+    <TransitionGroup name={props.animation} appear>
+      <For each={lyricTransitionGroup()}>
+        {(lyrics) => (
+          <Container>
+            <Lyrics lyrics={lyrics} style={props.lyricsStyle} status={props.lyricsStatus} />
+          </Container>
+        )}
+      </For>
+    </TransitionGroup>
+  );
+};
+
+const LyricsTransitionGroupSequential = (props: LyricsTransitionGroupProps) => {
+  const Container = untrack(() => props.container);
+
+  return (
+    <Container>
+      <TransitionGroup name={props.animation} appear>
+        <Lyrics lyrics={props.lyrics} style={props.lyricsStyle} status={props.lyricsStatus} />
+      </TransitionGroup>
+    </Container>
+  );
+};
+
+type LyricTransitionProps = JSX.HTMLAttributes<HTMLDivElement> & {
+  lyrics: string[];
+  status: Status;
+};
+
+const LyricsTransition = (props: LyricTransitionProps) => {
+  const [, lyricsProps, passedProps] = splitProps(props, ['class'], ['lyrics', 'status']);
+
+  const animation = () => {
+    const configuredName = config()?.style?.animation ?? 'pretty';
+    if (configuredName === 'custom') {
+      return userCSSTransitions['transition-lyric'];
+    }
+
+    return `lyric-${configuredName}`;
+  };
+
+  const [config] = useConfig();
+  const lyricsStyle = createMemo(on(config, (configData) => `
+    ${configData ? `
+      font-family: ${configData.style.font};
+      font-weight: ${configData.style.fontWeight};
+      font-size: ${configData.style.lyric.fontSize}px;
+      color: ${configData.style.lyric.color};
+      background-color: ${configData.style.lyric.background};
+    ` : ''};
+    text-align: var(--text-align);
+  `));
+
+  const Container = (containerProps: { children: JSX.Element }) => (
+    <div class={cx('flex flex-col items-end gap-4', userCSSSelectors.lyrics, props.class)} {...passedProps}>
+      {containerProps.children}
+    </div>
+  );
+
+  const LyricsTransitionGroup = (transitionProps: LyricsTransitionGroupProps) => (
+    <Show
+      when={config()?.style.animationAtOnce}
+      fallback={<LyricsTransitionGroupAllAtOnce {...transitionProps} />}
+    >
+      <LyricsTransitionGroupSequential {...transitionProps} />
+    </Show>
+  );
+
+  return (
+    <LyricsTransitionGroup 
+      animation={animation()}
+      container={Container}
+      lyrics={lyricsProps.lyrics}
+      lyricsStatus={lyricsProps.status}
+      lyricsStyle={lyricsStyle()}
+    />
+  );
+};
+
+export default LyricsTransition;
