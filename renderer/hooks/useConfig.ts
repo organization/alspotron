f2e70b01f0247dcad76f92db081971292b19dd0d
JellyBrick <shlee1503@naver.com>
2023-07-29 08:22:27 +0900

    useConfig에서 onMount로 초기화하게 변경

--- renderer/hooks/useConfig.ts
+++ renderer/hooks/useConfig.ts
@@ -1,17 +1,17 @@
-import { createSignal } from 'solid-js';
+import { createSignal, onMount } from 'solid-js';
 
 import { Config } from '../../src/config';
 
 const useConfig = () => {
   const [config, setConfig] = createSignal<Config | null>(null);
 
-  (() => {
+  onMount(() => {
     if (config()) return;
 
     const result = window.ipcRenderer.sendSync('get-config') as Config;
 
     setConfig(result);
-  })();
+  });
 
   window.ipcRenderer.on('config', (_, data: Config) => {
     setConfig(data);
