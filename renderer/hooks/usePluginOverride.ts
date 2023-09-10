9489cf3c294501f2edb1b07c78baf17219658e09
Su-Yong <simssy2205@gmail.com>
2023-09-10 22:34:13 +0900

    feat(plugin): improve plugin performance

--- renderer/hooks/usePluginOverride.ts
+++ renderer/hooks/usePluginOverride.ts
@@ -1,3 +1,5 @@
+import usePlugins from './usePlugins';
+
 import { OverrideMap, OverrideParameterMap } from '../../common/plugin';
 
 const usePluginOverride = async <Target extends keyof OverrideMap>(
@@ -5,9 +7,16 @@ const usePluginOverride = async <Target extends keyof OverrideMap>(
   originalFn: (...args: OverrideParameterMap[Target]) => Promise<void> | void,
   ...args: OverrideParameterMap[Target]
 ): Promise<void> => {
-  const result = await window.ipcRenderer.invoke('override-plugin', target, ...args) as false | OverrideParameterMap[Target];
+  const { plugins } = usePlugins();
 
-  if (result !== false) await originalFn(...result);
+  const isEnabled = plugins().some((plugin) => plugin.state === 'enable');
+  if (isEnabled) {
+    const result = await window.ipcRenderer.invoke('override-plugin', target, ...args) as false | OverrideParameterMap[Target];
+    
+    if (result !== false) await originalFn(...result);
+  } else {
+    await originalFn(...args);
+  }
 };
 
 export default usePluginOverride;
