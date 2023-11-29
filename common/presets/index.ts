1f1d9b4ba97ddfd9a464d2e807b116a4256220b7
Su-Yong <simssy2205@gmail.com>
2023-11-29 23:51:36 +0900

    feat(theme): add `Dynamic Island` theme preset

--- common/presets/index.ts
+++ common/presets/index.ts
@@ -1,5 +1,6 @@
 import albumCover from './album-cover.json';
 import multipleLyric from './multiple-lyric.json';
+import dynamicIsland from './dynamic-island.json';
 
 import { DEFAULT_STYLE } from '../constants';
 import { StyleConfig } from '../schema';
@@ -8,4 +9,5 @@ export default {
   'default': DEFAULT_STYLE,
   'album-cover': albumCover,
   'multiple-lyric': multipleLyric,
+  'dynamic-island': dynamicIsland,
 } as Record<string, StyleConfig>;
