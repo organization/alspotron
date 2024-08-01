915ad4fec1f85a2dc53d93b586c6ceb966f2d085
Su-Yong <simssy2205@gmail.com>
2024-08-01 21:48:26 +0900

    feat(preset): update Album Cover theme and add Overwatch theme

--- common/presets/index.ts
+++ common/presets/index.ts
@@ -1,6 +1,7 @@
 import albumCover from './album-cover.json';
 import multipleLyric from './multiple-lyric.json';
 import dynamicIsland from './dynamic-island.json';
+import overwatch from './overwatch.json';
 
 import { DEFAULT_STYLE } from '../constants';
 import { StyleConfig } from '../schema';
@@ -10,4 +11,5 @@ export default {
   'album-cover': albumCover,
   'multiple-lyric': multipleLyric,
   'dynamic-island': dynamicIsland,
+  'overwatch': overwatch,
 } as Record<string, StyleConfig>;
