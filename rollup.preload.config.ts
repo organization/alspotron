269503f0f6f0dc23817093037484e752fe003221
Khinenw <deu07115@gmail.com>
2024-12-01 14:46:48 +0900

    feat: use swc and optimize dev speed

--- rollup.preload.config.ts
+++ rollup.preload.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'rollup';
 import builtinModules from 'builtin-modules';
-import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from "@rollup/plugin-node-resolve";
 import json from '@rollup/plugin-json';
+import swc from '@rollup/plugin-swc';
 import terser from '@rollup/plugin-terser';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   plugins: [
-    typescript({
-      module: 'ESNext',
-    }),
-    nodeResolve({ browser: false }),
-    commonjs(),
     json(),
-    terser({
+    swc({ exclude: [/node_modules/] }),
+    nodeResolve({ browser: false }),
+    commonjs({ extensions: ['.js', '.ts'] }),
+    isProduction && terser({
       ecma: 2020,
     }),
     {
