f54dac994987488d881e0c0438b20685b96baaf8
Khinenw <deu07115@gmail.com>
2024-12-04 18:32:06 +0900

    feat(tray): implement tray menu

--- common/utils.ts
+++ common/utils.ts
@@ -1,1 +1,6 @@
+/**
+ * @deprecated use PlayingInfo::id
+ * @description FIXME SourceProviders already calculate their ids.
+ *    And using this can lead to bug when SourceProviders change their id scheme.
+ */
 export const getLyricMapperId = (title: string, coverUrl: string | null = 'unknown') => `${title}:${coverUrl ?? 'unknown'}`;
